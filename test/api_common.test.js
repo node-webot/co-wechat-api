5a6603821d1d53a1ab030fb463cd01500d82e558
Jackson Tian <shyvo1987@gmail.com>
2017-04-08 16:00:34 +0800

    use async/await

--- test/api_common.test.js
+++ test/api_common.test.js
@@ -30,16 +30,16 @@ describe('api_common', function () {
   });
 
   describe('getAccessToken', function () {
-    it('should ok', function* () {
+    it('should ok', async function () {
       var api = new API(config.appid, config.appsecret);
-      var token = yield api.getAccessToken();
+      var token = await api.getAccessToken();
       expect(token).to.only.have.keys('accessToken', 'expireTime');
     });
 
-    it('should not ok', function* () {
+    it('should not ok', async function () {
       var api = new API('appid', 'secret');
       try {
-        yield api.getAccessToken();
+        await api.getAccessToken();
       } catch (err) {
         expect(err).to.have.property('name', 'WeChatAPIError');
         expect(err).to.have.property('message', 'invalid credential');
